4273a0911a3836b721c44e10e7313f6ed4b9da28
DuvanSilva <duvan.silva.dev@gmail.com>
2025-07-26 21:20:37 -0500

    fron-end advance

---
+++ script.js
@@ -0,0 +1,5 @@
+const Tarea = document.getElementById("formTarea");
+const TablaTareas = document.getElementById("listaTareas");
+const InputTexto = document.getElementById("inputTarea")
+
+const TareasGuardadas = localStorage.getItem("textoTarea");
