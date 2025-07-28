69f7d0f9cc2f43005803806f2ada0132f712c09a
DuvanSilva <duvan.silva.dev@gmail.com>
2025-07-27 23:50:06 -0500

    V2. Functionality advancement

--- script.js
+++ script.js
@@ -2,4 +2,23 @@ const Tarea = document.getElementById("formTarea");
 const TablaTareas = document.getElementById("listaTareas");
 const InputTexto = document.getElementById("inputTarea")
 
-const TareasGuardadas = localStorage.getItem("textoTarea");
+let tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+
+function SaveTareas (){
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+};
+
+function AggTarea(textTarea) {
+    const nuevaTarea = {
+        texto: textTarea,
+        completado: false
+    };
+    tareas.push(nuevaTarea);
+    SaveTareas();
+}
+
+function DelTarea(textTarea){
+
+}
+
+
